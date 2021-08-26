e54a87eb62bab13b7b9dd2e57806017a10473dc0
Cameron Walker <cwalker33@huskers.unl.edu>
2021-08-26 17:42:38 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+    * Author: Cameron Walker
+    * Date: 2021/09/24
+    * 
+      A simple hello world program in C 
+    *
+*/
+#include <stdlib.h> 
+#include <stdio.h>
+
+   int main(int argc, char **argv) {
+       
+     printf("Cameron Walker\n");
+     
+return 0; 
+   }
