31f35c5048b690040c7802b1512c24bfbfcfdf4a
Cameron Walker <cwalker33@huskers.unl.edu>
2021-08-26 18:17:47 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
     * Author: Cameron Walker
     * Date: 2021/09/24
-    * 
-      A simple hello world program in C 
+    *
+      A simple hello world program in C
     *
 */
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <stdio.h>
 
    int main(int argc, char **argv) {
-       
-     printf("Cameron Walker\n");
-     
-return 0; 
+
+     printf("Cameron Walker!\n");
+     printf("Computer Science\n")
+return 0;
    }
